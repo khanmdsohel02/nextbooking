125ffc7fbe6fcd0b6aafcafe768c53d13bf08754
khanmdsohel02 <khanmdsohel02.25@gmail.com>
2024-06-15 13:57:39 +0600

    try error solving

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    mongoose.connect(process.env.MONGODB_URL);
+    await mongoose.connect(process.env.MONGODB_URL);
     console.log("MongoDB connected");
   } catch (error) {
     console.log(error);
