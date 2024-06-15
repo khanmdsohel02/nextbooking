a8f18ce46853f407900e39a26b3d89779c4a900d
khanmdsohel02 <khanmdsohel02.25@gmail.com>
2024-06-16 00:38:20 +0600

    try err

--- controllers/auth.js
+++ controllers/auth.js
@@ -29,10 +29,8 @@ export const login = async (req, res, next) => {
     const user = await User.findOne({ username: req.body.username });
     if (!user) return next(createError(404, "User not found"));
 
-    const isPasswordCorrect = await bcrypt.compare(
-      req.body.password,
-      user.password
-    );
+    const isPasswordCorrect = req.body.password === user.password;
+
     if (!isPasswordCorrect) return next(createError(400, "Wrong password"));
 
     const token = jwt.sign(
